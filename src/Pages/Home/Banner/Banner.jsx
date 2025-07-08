acc2a4d6c98ea26d6261f7545afe0b829d3a7df9
abdulkader33447 <abdulkader33447@gmail.com>
2025-07-08 20:48:45 +0600

    added slider with title and subtitle

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,100 +1,74 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import imgOne from "../../../assets/pexels-life-of-pix-8169.jpg";
+import imgTwo from "../../../assets/pexels-yankrukov-7698819.jpg";
+import imgThree from "../../../assets/pexels-alphatradezone-5833793.jpg";
+
+const slides = [
+  {
+    img: imgOne,
+    title: "Complete Micro Tasks Effortlessly",
+    subtitle: "Earn coins by completing small tasks from real buyers.",
+  },
+  {
+    img: imgTwo,
+    title: "Buyers, Get Things Done Fast",
+    subtitle: "Post your task, set reward, and pay only when it's approved.",
+  },
+  {
+    img: imgThree,
+    title: "Track Earnings and Withdraw Anytime",
+    subtitle: "View your balance, submit proof, and withdraw instantly.",
+  },
+];
 
 const Banner = () => {
   return (
-    <div className="bg-blue-50">
-      <div className="lg:w-8/12 md:w-9/12 w-11/12 mx-auto">
-      <h1>banner</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-        dignissimos accusamus ipsa velit autem ea, officiis porro minima cum,
-        labore voluptates non cumque sunt aut numquam vitae animi nemo?
-        Perspiciatis in cupiditate, quia ratione officia nisi dolorum ipsam
-        earum debitis eveniet fuga illum accusantium, quos natus maxime quod
-        distinctio repudiandae, officiis aut totam expedita inventore quas! Sint
-        iure totam fugiat porro quod illo nam nihil, dolores voluptatibus
-        possimus similique, facere labore vel ex magnam maiores temporibus ullam
-        dignissimos quis repellat. Eius recusandae architecto error minus odit
-        dolore a. Nisi corrupti culpa officia fugit rem atque, at ullam
-        voluptatibus itaque enim aliquid modi sapiente minima, libero eligendi
-        vel sit sequi quidem iure aspernatur debitis dignissimos doloribus,
-        tenetur quae? Perspiciatis hic, nesciunt aliquam fugiat ducimus iste
-        dignissimos praesentium sed fuga quae eum tempora laudantium optio.
-        Enim, odio cum, voluptate repellat magni iure ab neque officia sed eaque
-        distinctio totam consectetur doloribus consequuntur laborum animi soluta
-        sequi necessitatibus a. Earum quos soluta architecto voluptates ex minus
-        provident mollitia itaque doloremque amet hic doloribus culpa, fuga rem
-        dolorum ab delectus alias impedit veritatis sint accusamus voluptatum
-        sed! Perspiciatis dolor ab quisquam illo provident repellat officiis
-        asperiores eveniet, dolore rem excepturi blanditiis nisi corrupti ad
-        nobis corporis! Odio, dolore inventore! Facere dignissimos autem
-        corrupti quasi eius quae incidunt commodi veniam magnam modi deleniti
-        perferendis quia vero, consectetur eligendi doloremque dolorum atque
-        nemo! Voluptate pariatur quisquam ullam sed eum illum quae odio expedita
-        sint repellat, consequatur recusandae eveniet, distinctio, facere
-        excepturi doloribus odit. Incidunt quis, quasi omnis illum dolore fugit
-        necessitatibus deserunt nam asperiores suscipit. Eveniet ipsum obcaecati
-        sit earum repellendus accusantium ab mollitia vero reiciendis. Doloribus
-        nihil voluptatum modi repellat incidunt quae minus, enim soluta eius!
-        Accusamus corporis hic omnis minima blanditiis, animi officiis expedita
-        laborum atque? Provident, architecto eveniet velit laboriosam repellat
-        commodi quisquam adipisci non minus temporibus voluptates reiciendis
-        dolorum omnis voluptatum magnam maxime eaque delectus quasi mollitia
-        libero? Laboriosam iste aliquid ea itaque, eveniet ipsa error!
-        Repellendus nemo cumque excepturi deleniti quos fugiat, perspiciatis
-        nesciunt voluptatum reiciendis ad et omnis, deserunt ducimus iure
-        repudiandae esse unde eligendi sunt architecto? Animi voluptatem et
-        temporibus eius excepturi quos quam tenetur itaque consequuntur
-        exercitationem expedita at perferendis ipsam quidem voluptatum earum
-        dignissimos repudiandae enim quia nobis, beatae fugit? Esse saepe dicta
-        facilis deserunt deleniti ea non rerum voluptatum soluta laborum omnis
-        eum tempora consequatur id dolorem quis dolor, labore nesciunt ab odio
-        commodi atque blanditiis! Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Error dignissimos accusamus ipsa velit autem ea,
-        officiis porro minima cum, labore voluptates non cumque sunt aut numquam
-        vitae animi nemo? Perspiciatis in cupiditate, quia ratione officia nisi
-        dolorum ipsam earum debitis eveniet fuga illum accusantium, quos natus
-        maxime quod distinctio repudiandae, officiis aut totam expedita
-        inventore quas! Sint iure totam fugiat porro quod illo nam nihil,
-        dolores voluptatibus possimus similique, facere labore vel ex magnam
-        maiores temporibus ullam dignissimos quis repellat. Eius recusandae
-        architecto error minus odit dolore a. Nisi corrupti culpa officia fugit
-        rem atque, at ullam voluptatibus itaque enim aliquid modi sapiente
-        minima, libero eligendi vel sit sequi quidem iure aspernatur debitis
-        dignissimos doloribus, tenetur quae? Perspiciatis hic, nesciunt aliquam
-        fugiat ducimus iste dignissimos praesentium sed fuga quae eum tempora
-        laudantium optio. Enim, odio cum, voluptate repellat magni iure ab neque
-        officia sed eaque distinctio totam consectetur doloribus consequuntur
-        laborum animi soluta sequi necessitatibus a. Earum quos soluta
-        architecto voluptates ex minus provident mollitia itaque doloremque amet
-        hic doloribus culpa, fuga rem dolorum ab delectus alias impedit
-        veritatis sint accusamus voluptatum sed! Perspiciatis dolor ab quisquam
-        illo provident repellat officiis asperiores eveniet, dolore rem
-        excepturi blanditiis nisi corrupti ad nobis corporis! Odio, dolore
-        inventore! Facere dignissimos autem corrupti quasi eius quae incidunt
-        commodi veniam magnam modi deleniti perferendis quia vero, consectetur
-        eligendi doloremque dolorum atque nemo! Voluptate pariatur quisquam
-        ullam sed eum illum quae odio expedita sint repellat, consequatur
-        recusandae eveniet, distinctio, facere excepturi doloribus odit.
-        Incidunt quis, quasi omnis illum dolore fugit necessitatibus deserunt
-        nam asperiores suscipit. Eveniet ipsum obcaecati sit earum repellendus
-        accusantium ab mollitia vero reiciendis. Doloribus nihil voluptatum modi
-        repellat incidunt quae minus, enim soluta eius! Accusamus corporis hic
-        omnis minima blanditiis, animi officiis expedita laborum atque?
-        Provident, architecto eveniet velit laboriosam repellat commodi quisquam
-        adipisci non minus temporibus voluptates reiciendis dolorum omnis
-        voluptatum magnam maxime eaque delectus quasi mollitia libero?
-        Laboriosam iste aliquid ea itaque, eveniet ipsa error! Repellendus nemo
-        cumque excepturi deleniti quos fugiat, perspiciatis nesciunt voluptatum
-        reiciendis ad et omnis, deserunt ducimus iure repudiandae esse unde
-        eligendi sunt architecto? Animi voluptatem et temporibus eius excepturi
-        quos quam tenetur itaque consequuntur exercitationem expedita at
-        perferendis ipsam quidem voluptatum earum dignissimos repudiandae enim
-        quia nobis, beatae fugit? Esse saepe dicta facilis deserunt deleniti ea
-        non rerum voluptatum soluta laborum omnis eum tempora consequatur id
-        dolorem quis dolor, labore nesciunt ab odio commodi atque blanditiis!
-      </p>
-    </div>
+    <div className="bg-[#1ebcec0b]">
+      <div className="lg:w-8/12 md:w-10/12 w-11/12 mx-auto lg:py-20 md:py-14 py-10">
+        <div className="pb-8">
+          <h1 className="text-center lg:text-5xl md:text-3xl text-2xl font-semibold text-gray-800">
+            Discover Tasks, Earn Coins, Level Up!
+          </h1>
+          <p className="sm:w-96 w-11/12 mx-auto text-center mt-2">
+            Join thousands of workers completing micro tasks and getting
+            rewarded instantly
+          </p>
+        </div>
+
+        {/* Slider */}
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showThumbs={false}
+          showIndicators={true}
+          interval={4000}
+          showStatus={false}
+        >
+          {slides.map((slide, idx) => (
+            <div key={idx} className="relative group rounded-md">
+              <img
+                src={slide.img}
+                alt={slide.title}
+                className="lg:h-[600px] md:h-[400px] h-[250px] w-full object-cover rounded-md"
+              />
+              {/* Overlay */}
+              <div
+                className="absolute inset-0 bg-gradient-to-r from-black/60 to-black/60 z-50 
+             flex flex-col justify-center items-center text-center px-4 text-white rounded-md"
+              >
+                <h2 className="text-xl md:text-3xl lg:text-4xl font-bold mb-2 drop-shadow-lg">
+                  {slide.title}
+                </h2>
+                <p className="text-sm md:text-lg lg:text-xl mb-4 drop-shadow">
+                  {slide.subtitle}
+                </p>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 };
