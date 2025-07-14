9af1e278a2fc4f3a2f149454018f8eec8b44e7ff
abdulkader33447 <abdulkader33447@gmail.com>
2025-07-14 20:27:39 +0600

    impliment delete function

--- src/Pages/Dashboard/MyTasks/MyTasks.jsx
+++ src/Pages/Dashboard/MyTasks/MyTasks.jsx
@@ -4,6 +4,7 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import LoadingSpinner from "../../LoadingSpinner/LoadingSpinner";
 import { MdDeleteOutline } from "react-icons/md";
 import { FiEdit } from "react-icons/fi";
+import Swal from "sweetalert2";
 
 const MyTasks = () => {
   const { user } = useAuth();
@@ -23,6 +24,34 @@ const MyTasks = () => {
     return <LoadingSpinner />;
   }
   // console.log(tasks);
+
+  const handleDelete = (taskId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await axiosSecure.delete(`/tasks/${taskId}`);
+          if (res.data.success) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            refetch()
+          }
+        } catch (error) {
+          Swal.fire("Error", "Faild to delete task.", error);
+        }
+      }
+    });
+  };
   return (
     <div>
       <h1>MyTasks</h1>
@@ -49,10 +78,10 @@ const MyTasks = () => {
                   <td>{task.completion_date}</td>
                   <td className="space-x-2">
                     <button>
-                      <FiEdit className="inline-block mr-2 cursor-pointer size-5 text-blue-600" />
+                      <FiEdit className="inline-block mr-2 cursor-pointer size-5 text-blue-600 hover:size-6" />
                     </button>
-                    <button>
-                      <MdDeleteOutline className="inline-block mr-2 text-red-600 size-6 cursor-pointer" />
+                    <button onClick={() => handleDelete(task._id)}>
+                      <MdDeleteOutline className="inline-block mr-2 text-red-600 size-6 cursor-pointer hover:size-7" />
                     </button>
                   </td>
                 </tr>
